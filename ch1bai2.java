f16cfaeaae4e6479c575b4a75ca7e89f70842ea5
mimic0n <kimthonglord@gmail.com>
2024-09-24 00:54:29 +0700

    BTVN1

---
+++ ch1bai2.java
@@ -0,0 +1,21 @@
+package xin_chao;
+import java.util.Scanner;
+
+public class ch1bai2  {
+
+	
+
+	public static void main(String[] args) {
+		Scanner k=new Scanner(System.in);
+		System.out.print("nhap vao mot chuoi bat ki:");
+		String str;
+		str = k.next();
+		System.out.print("do dai cua chuoi la :" + str.length());
+		
+	}
+		
+	
+	
+	
+
+}
